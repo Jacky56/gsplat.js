0933db9fa039b42c38a6911cad767ef088867afa
Jacky Banh Ha <jackybanh1997@gmail.com>
2023-12-03 11:42:41 +0000

    add control paste
    Signed-off-by: Jacky Banh Ha <jackybanh1997@gmail.com>

--- gsplat/src/main.ts
+++ gsplat/src/main.ts
@@ -4,6 +4,7 @@ const renderer = new SPLAT.WebGLRenderer();
 const scene = new SPLAT.Scene();
 const camera = new SPLAT.Camera();
 const controls = new SPLAT.OrbitControls(camera, renderer.domElement);
+const progressIndicator = document.getElementById("progress-indicator") as HTMLProgressElement;
 
 let loading = false;
 
@@ -12,22 +13,41 @@ async function selectFile(file: File) {
     loading = true;
     // Check if .splat file
     if (file.name.endsWith(".splat")) {
-        await SPLAT.Loader.LoadFromFileAsync(file, scene, (progress: number) => {
-            console.log("Loading SPLAT file: " + progress);
-        });
+        await SPLAT.Loader.LoadFromFileAsync(file, scene, loader);
     } else if (file.name.endsWith(".ply")) {
         const format = "";
         // const format = "polycam"; // Uncomment to load a Polycam PLY file
         await SPLAT.PLYLoader.LoadFromFileAsync(
             file,
             scene,
-            (progress: number) => {
-                console.log("Loading PLY file: " + progress);
-            },
+            loader,
             format,
         );
     }
     loading = false;
+}
+
+async function selectUrl(url: string) {
+    if (loading) return;
+    loading = true;
+    // Check if .splat file
+    if (url.endsWith(".splat")) {
+        await SPLAT.Loader.LoadAsync(url, scene, loader);
+    } else if (url.endsWith(".ply")) {
+        const format = "";
+        // const format = "polycam"; // Uncomment to load a Polycam PLY file
+        await SPLAT.PLYLoader.LoadAsync(
+            url,
+            scene,
+            loader,
+            format,
+        );
+    }
+    loading = false;
+}
+
+function loader(progress: number) {
+    progressIndicator.value = progress * 100;
 }
 
 async function main() {
@@ -45,6 +65,7 @@ async function main() {
 
     requestAnimationFrame(frame);
 
+    try {
     // Listen for file drops
     document.addEventListener("drop", (e) => {
         e.preventDefault();
@@ -54,6 +75,19 @@ async function main() {
             selectFile(e.dataTransfer.files[0]);
         }
     });
+    
+    document.addEventListener("paste", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        let url = e.clipboardData?.getData("text").trim();
+
+        if ((url != null) && (url.endsWith(".ply") || url.endsWith(".splat"))) {
+            selectUrl(url);
+        }
+    });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 main();
